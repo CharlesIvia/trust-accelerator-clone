10b1ae31843d159ed5950468b0782482779f825f
Charles Ivia <iviacharles@gmail.com>
2020-07-15 17:17:06 +0300

    Created WhatItTakes.js component

---
+++ src/components/WhatItTakes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import progress from "../images/image01.png";
+
+const WhatItTakes = () => {
+  return (
+    <div>
+      <h1>What it takes</h1>
+      <img className="progress-img" src={progress} alt="" />
+    </div>
+  );
+};
+
+export default WhatItTakes;
