1ac6f660d0d269542185489c8874f64a8632cc2e
Charles Ivia <iviacharles@gmail.com>
2020-07-15 16:25:52 +0300

    Created and added Influence.js component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
+import Influence from "./components/Influence";
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Contact />
+      <Influence />
     </div>
   );
 }
---
+++ src/components/Influence.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Influence = () => {
+  return (
+    <div>
+      <div>
+        <p>Influence</p>
+        <h2>Everywhere.</h2>
+      </div>
+
+      <div>
+        <p>We build content engines fueled exclusively by voice, so you can:</p>
+        <ul>
+          <li>Spend less time marketing and more time innovating.</li>
+          <li>
+            Strengthen your brand, your network and secure bigger opportunities.
+          </li>
+          <li>
+            Get your content in front of more people in ways they like to
+            consume it.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Influence;
