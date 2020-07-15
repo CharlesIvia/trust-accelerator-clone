86f7b9b726a46b814c26dbab277477136a119be3
Charles Ivia <iviacharles@gmail.com>
2020-07-15 13:22:01 +0300

    Created and added Contact.js component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Hero from "./components/Hero";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <div>
+        <h2>Want to know how we do it?</h2>
+        <p>Join our free list for guides, templates and insights.</p>
+      </div>
+
+      <div>
+        <form>
+          <input type="text" placeholder="Email" />
+          <button type="submit">Join the List</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
