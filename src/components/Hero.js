1bde96fb7114e51e9cf9e89b8594c4945d0cf5bd
Charles Ivia <iviacharles@gmail.com>
2020-07-17 19:58:17 +0300

    Updated Hero component markup with  class names

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,14 +2,14 @@ import React from "react";
 import brain from "../images/big-brain.png";
 const Hero = () => {
   return (
-    <div>
-      <h1>TRUST ACCELERATOR</h1>
-      <div>
-        <p>We build engines for entrepreneurs.</p>
-        <img src={brain} alt="big-brain" />
+    <div className="container hero ">
+      <h1 className="title">TRUST ACCELERATOR</h1>
+      <div className="tag-line">
+        <p className="tag">We build content engines for entrepreneurs.</p>
+        <img className="big-brain" src={brain} alt="big-brain" />
       </div>
       <div className="line"></div>
-      <p>
+      <p className="do">
         We help businesses create and nurture long-term relationships with their
         future customers.
       </p>
