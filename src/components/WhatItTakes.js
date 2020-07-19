e57ed199f775b1c7542e67f2a19d5b2e0069b3c9
Charles Ivia <iviacharles@gmail.com>
2020-07-19 13:33:56 +0300

    Updated component's markup

--- src/components/WhatItTakes.js
+++ src/components/WhatItTakes.js
@@ -3,8 +3,8 @@ import progress from "../images/image01.png";
 
 const WhatItTakes = () => {
   return (
-    <div>
-      <h1>What it takes</h1>
+    <div className="container takes">
+      <h1 className="what">What it takes</h1>
       <img className="progress-img" src={progress} alt="" />
     </div>
   );
