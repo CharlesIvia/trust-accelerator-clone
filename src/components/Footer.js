9686c46e5cb2935c71f2437f8f09491757d3c510
Charles Ivia <iviacharles@gmail.com>
2020-07-19 15:17:06 +0300

    Updated component's markup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,27 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="container footer">
-      <div className="first-chunk">
-        <p className="dash">_</p>
-        <ul>
-          <li>
-            <a href="#home">LinkedIn</a>
-          </li>
-          <li>
-            <a href="#home">Facebook</a>
-          </li>
-          <li>
-            <a href="#home">Instagram</a>
-          </li>
-        </ul>
-      </div>
+      <div className="footer-items">
+        <div className="first-chunk">
+          <p className="dash">_</p>
+          <ul className="listed">
+            <li>
+              <a href="#home">LinkedIn</a>
+            </li>
+            <li>
+              <a href="#home">Facebook</a>
+            </li>
+            <li>
+              <a href="#home">Instagram</a>
+            </li>
+          </ul>
+        </div>
 
-      <div className="second-chunk">
-        <p className="dash">_</p>
-        <p> &copy; 2019 The Trust Accelerator.</p>
-        <p>All rights reserved.</p>
+        <div className="second-chunk">
+          <p className="dash">_</p>
+          <p> &copy; 2019 The Trust Accelerator.</p>
+          <p>All rights reserved.</p>
+        </div>
       </div>
     </div>
   );
