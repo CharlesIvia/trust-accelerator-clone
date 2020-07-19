00d87454f9871974c28cfcaf5f599b9c2bbac8ba
Charles Ivia <iviacharles@gmail.com>
2020-07-19 15:35:07 +0300

    Updates

---
+++ precache-manifest.4dea10244112cbb48728222afbee0f22.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6378939d1e5cc34514da4c4938e7648",
+    "url": "/trust-accelerator-clone/index.html"
+  },
+  {
+    "revision": "802c636b98f487b8cd23",
+    "url": "/trust-accelerator-clone/static/css/main.fd3152bf.chunk.css"
+  },
+  {
+    "revision": "7fba969f71b278e380f5",
+    "url": "/trust-accelerator-clone/static/js/2.1a5f2c10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trust-accelerator-clone/static/js/2.1a5f2c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802c636b98f487b8cd23",
+    "url": "/trust-accelerator-clone/static/js/main.827b194f.chunk.js"
+  },
+  {
+    "revision": "5d284747fdd5d6c15feb",
+    "url": "/trust-accelerator-clone/static/js/runtime-main.6ffbb464.js"
+  },
+  {
+    "revision": "07d5c8068dac1373fe41005544d5f8a2",
+    "url": "/trust-accelerator-clone/static/media/big-brain.07d5c806.png"
+  },
+  {
+    "revision": "c4cbb894786f0e8d794d622195093d5e",
+    "url": "/trust-accelerator-clone/static/media/brain.c4cbb894.svg"
+  },
+  {
+    "revision": "580b7105cab44fd1a474ac4fbe41e150",
+    "url": "/trust-accelerator-clone/static/media/chalo.580b7105.jpg"
+  },
+  {
+    "revision": "b5016e80820f0ef1a42b1474c6adefc4",
+    "url": "/trust-accelerator-clone/static/media/image01.b5016e80.png"
+  },
+  {
+    "revision": "91584302ae0b27fa1a6247106be04604",
+    "url": "/trust-accelerator-clone/static/media/image05.91584302.svg"
+  },
+  {
+    "revision": "d7517d6d1eebbd457a2d8a724ee6df0a",
+    "url": "/trust-accelerator-clone/static/media/image06.d7517d6d.svg"
+  },
+  {
+    "revision": "1af68fd4e7911732cc37dd4cd14e7b1a",
+    "url": "/trust-accelerator-clone/static/media/image08.1af68fd4.svg"
+  },
+  {
+    "revision": "10a565b464c980e49242a9d7b16d7754",
+    "url": "/trust-accelerator-clone/static/media/image10.10a565b4.jpg"
+  },
+  {
+    "revision": "5f637145bb88e645005d8d3794969b1f",
+    "url": "/trust-accelerator-clone/static/media/image11.5f637145.jpg"
+  },
+  {
+    "revision": "2cbcd8c859ad9070f3b924e71c88a10d",
+    "url": "/trust-accelerator-clone/static/media/image12.2cbcd8c8.jpg"
+  },
+  {
+    "revision": "8b3a80a2284bc0042210ed276b79251e",
+    "url": "/trust-accelerator-clone/static/media/strategic.8b3a80a2.jpg"
+  }
+]);
